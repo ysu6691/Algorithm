8639c9e00ebdb801ee4f8918b55397bf92d1e521
ysu6691 <ysu6691@gmail.com>
2022-09-28 20:31:00 +0900

    220928

--- SWEA/D4/SWEA_5247.py
+++ SWEA/D4/SWEA_5247.py
@@ -3,14 +3,16 @@ testcase = int(input())
 for tc in range(1, testcase+1):
 
     N, M = map(int, input().split())
-    num_list = [[N]]
-    memo = {N}
+    # num_list의 n번째 인덱스 리스트는 n번 연산했을 때 만들 수 있는 숫자 모음
+    num_list = [[N]] # 0번 연산했을 때 만들 수 있는 숫자는 N 하나뿐
+    memo = {N} # 연산해서 나왔던 숫자 기억
 
     cnt = 1
 
     while True:
-        num_list.append([])
-        for num in num_list[cnt-1]:
+        num_list.append([]) # 다음 연산으로
+        for num in num_list[cnt-1]: # 이전 연산만 보면 됨
+            # 연산 후 이미 나왔던 숫자가 아니면, 기억하기
             if num + 1 not in memo and num + 1 <= 1000000:
                 num_list[cnt].append(num+1)
                 memo.add(num+1)
@@ -24,9 +26,11 @@ for tc in range(1, testcase+1):
                 num_list[cnt].append(num-10)
                 memo.add(num-10)
 
+        # 원하는 숫자 찾았으면 break
         if M in num_list[cnt]:
             break
 
+        # 다음 연산으로
         cnt += 1
 
     print(f'#{tc} {cnt}')

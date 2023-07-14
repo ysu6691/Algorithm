57036749d81d1e5403df9c4636f586442a7aaa2b
ysu6691 <ysu6691@gmail.com>
2023-07-14 23:25:26 +0900

    230714

--- BOJ/Gold4/BOJ_1647.py
+++ BOJ/Gold4/BOJ_1647.py
@@ -1,35 +1,27 @@
-def find_set(n):
-    if n != parent[n]:
-        n = find_set(parent[n])
-    return parent[n]
-
-def union(n1, n2):
-    n1 = find_set(n1)
-    n2 = find_set(n2)
-    if rank[n1] > rank[n2]:
-        parent[n2] = n1
-    elif rank[n2] > rank[n1]:
-        parent[n1] = n2
-    else:
-        parent[n2] = n1
-        rank[n1] += 1
+import sys, heapq
+input = sys.stdin.readline
 
 N, M = map(int, input().split())
-edges = [list(map(int, input().split())) for _ in range(M)]
-edges.sort(key=lambda x: x[2])
-parent = list(range(N+1))
-rank = [0]*(N+1)
-total_cost = 0
-cnt = 0
+adj_list = [[] for _ in range(N + 1)]
+for _ in range(M):
+    n1, n2, cost = map(int, input().split())
+    adj_list[n1].append((cost, n2))
+    adj_list[n2].append((cost, n1))
 
-for edge in edges:
-    n1, n2, cost = edge[0], edge[1], edge[2]
+INF = 1000001
+distance = [INF] * (N + 1)
+queue = [(0, 1)]
+visited = [0] * (N + 1)
+distance[1] = 0
 
-    if find_set(n1) != find_set(n2):
-        union(n1, n2)
-        total_cost += cost
-        cnt += 1
-        if cnt == N - 2:
-            break
-
-print(total_cost)
+while queue:
+    cost, current = heapq.heappop(queue)
+    if visited[current]:
+        continue
+    visited[current] = 1
+    for adj_info in adj_list[current]:
+        if not visited[adj_info[1]] and adj_info[0] < distance[adj_info[1]]:
+            distance[adj_info[1]] = adj_info[0]
+            heapq.heappush(queue, (adj_info[0], adj_info[1]))
+    
+print(sum(distance[1:]) - max(distance[1:]))
